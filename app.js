52a67535ffebe27da7ecea2dcd4bd6398fb2f1be
Christer Dale Montilla Reyes <rchristerdale@gmail.com>
2024-08-24 04:38:33 +0800

    bais

--- app.js
+++ app.js
@@ -1,60 +1,37 @@
 const express = require('express');
 const morgan = require('morgan');
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const cors = require('cors');
-const studentRouter = require('./routes/studentRoute');
 
-// Load environment variables from the config file
-dotenv.config({ path: './config.env' });
+// const hpp = require("hpp");
 
-// Create the Express app
 const app = express();
 
-// Middlewares
 app.use(cors());
+
+const studentRouter = require('./routes/studentRoute');
+
+// 1. Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
-
-// Custom middlewares to log requests and add request time
 app.use((req, res, next) => {
   console.log('Hello from the server 👋');
   next();
 });
-
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
-// Routes
+//3.) ROUTES
 app.use('/api/v1/student', studentRouter);
 
-app.get('/', (req, res) => {
-  res.redirect('/api/v1/student');
-});
-
-// Handle all undefined routes
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
 
-// Global error handling middleware
+app.use(globalErrorHandler);
 
-// Database connection
-const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
-  process.env.DATABASE_PASSWORD,
-);
-
-mongoose
-  .connect(DB)
-  .then(() => console.log('DB connection successful!'));
-
-// Start the server
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App is running on port ${port}...`);
-});
-
+// 4.) START SERVER
 module.exports = app;
--- models/studentModel.js
+++ models/studentModel.js
@@ -1,11 +1,36 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const studentSchema = new mongoose.Schema({
-    firstName: String,
-    lastName: String,
-    course: String,
-    year: String,
-    enrolled: Boolean,
+  studentName: {
+    type: String,
+    required: true,
+    maxlength: [
+      50,
+      'A student name must have less or equal than 50 characters',
+    ],
+    minlength: [
+      10,
+      'A student name must have more or equal than 10 characters',
+    ],
+  },
+  age: {
+    type: Number,
+    required: [true, 'A student should have age'],
+  },
+  section: {
+    type: String,
+    required: [true, 'A student should have a section'],
+  },
+  tuitionFee: {
+    type: Number,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+    select: false,
+  },
 });
 
-module.exports = mongoose.model('Student', studentSchema);
+const Student = mongoose.model('Students', studentSchema);
+
+module.exports = Student;
